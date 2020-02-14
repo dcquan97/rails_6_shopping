a61da72e6b06c9b98254b25a86c93e1b1b267410
YOUR NAME <YOUR@EMAIL.com>
2020-02-14 14:40:05 +0700

    done list product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ProductsController < ApplicationController
+  # before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  # GET /products
+  # GET /products.json
+  def index
+    @products = Product.all
+    # @order_item = current_user.cart_items.new
+  end
+
+  # GET /products/1
+  # GET /products/1.json
+  def show
+    @product = Product.find(params[:id])
+    session[:product_id] = @product.id
+  end
+end
