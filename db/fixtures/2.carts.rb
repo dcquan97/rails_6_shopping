f8f1cfbc42762ba7100b9267c4837f447ed7ffaf
YOUR NAME <YOUR@EMAIL.com>
2020-02-14 16:58:24 +0700

    add gem seed_fu & gem faker

---
+++ db/fixtures/2.carts.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+product_ids = Product.ids
+5.times do |_n|
+  User.all.each do |user|
+    ActiveRecord::Base.transaction do
+      u_cart = user.carts.create!(completed_at: Faker::Boolean.boolean)
+      u_cart.cart_items.create! product_id: product_ids.sample
+    end
+  end
+end
