e9154181cf059b75d58a4c062fe2bf2d73b31e47
YOUR NAME <YOUR@EMAIL.com>
2020-02-18 10:11:15 +0700

    done

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,7 @@ class CartsController < ApplicationController
   end
 
   def create
-    CartItem.create(cart_id: current_user.cart.id, product_id: params[:product_id])
+    current_user.cart.cart_items.create product_id: params[:product_id]
     redirect_to '/carts'
   end
 end
